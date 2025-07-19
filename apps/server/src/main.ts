90075c052a6daaf23978d6b1e6416b58e36f6001
Mathieu Morales <mathieumorales1612@gmail.com>
2025-07-19 15:33:28 +0200

    main

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,3 +5,24 @@ import { config } from './config';
 import userRouter from './resources/user.resource';
 import recipeRouter from './resources/recipe.resource';
 import { log } from './utils/logger';
+
+async function bootstrap() {
+
+  await mongoose.connect(config.mongoUri);
+  log('Connected to MongoDB');
+
+  const app = express();
+  app.use(bodyParser.json());
+
+  app.use('/users',   userRouter);
+  app.use('/recipes', recipeRouter);
+
+  app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    log(err.message);
+    res.status(500).json({ message: 'Erreur interne du serveur' });
+  });
+
+  app.listen(config.port, () => log(`Server listening on port ${config.port}`));
+}
+
+bootstrap().catch(err => console.error(err));
